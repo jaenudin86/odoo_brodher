96a114cb32b83c41519c53d0d6f943a4bb5a86f8
jaenudin <jaenudin@voksel.co.id>
2025-11-12 13:54:12 +0700

    q2323

--- brodher/__manifest__.py
+++ brodher/__manifest__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "brodher",
+    'name': "brodher_inv",
 
     'summary': "Short (1 phrase/line) summary of the module's purpose",
 
@@ -9,7 +9,7 @@ Long description of module's purpose
     """,
 
     'author': "My Company",
-    'website': "https://www.yourcompany.com",
+    'website': "https://jaenudin.com",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
